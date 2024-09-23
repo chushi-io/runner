40a91350126f50d1ba896b88d4e26ef03c6dc3da
Robert Wittman <robkwittman@gmail.com>
2024-09-22 22:25:28 -0400

    Add build.yml step

---
+++ go.mod
@@ -0,0 +1,42 @@
+module github.com/chushi-io/runner
+
+go 1.22.0
+
+toolchain go1.22.5
+
+require (
+	connectrpc.com/connect v1.17.0
+	github.com/chushi-io/chushi v1.13.0
+	github.com/hashicorp/go-tfe v1.66.0
+	github.com/hashicorp/go-version v1.7.0
+	github.com/hashicorp/terraform-exec v0.21.0
+	go.uber.org/zap v1.27.0
+	golang.org/x/net v0.29.0
+)
+
+require (
+	github.com/ProtonMail/go-crypto v1.1.0-alpha.2 // indirect
+	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
+	github.com/chushi-io/chushi-go-sdk v0.0.0-20240923005237-113cea6867c4 // indirect
+	github.com/chushi-io/hc-install v0.0.0-20240923012423-ccdcff4a3e26 // indirect
+	github.com/cloudflare/circl v1.3.7 // indirect
+	github.com/go-resty/resty/v2 v2.15.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/jsonapi v1.0.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
+	github.com/hashicorp/go-slug v0.16.0 // indirect
+	github.com/hashicorp/jsonapi v1.3.1 // indirect
+	github.com/hashicorp/terraform-json v0.22.1 // indirect
+	github.com/zclconf/go-cty v1.14.4 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/mod v0.21.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/time v0.6.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"github.com/chushi-io/chushi-go-sdk"
+	install "github.com/chushi-io/hc-install"
+	"github.com/chushi-io/hc-install/product"
+	"github.com/chushi-io/hc-install/releases"
+	"github.com/chushi-io/hc-install/src"
+	"github.com/hashicorp/go-tfe"
+	"github.com/hashicorp/go-version"
+	"github.com/hashicorp/terraform-exec/tfexec"
+	"go.uber.org/zap"
+	"io"
+	"os"
+	"strings"
+)
+
+var logger *zap.Logger
+
+func init() {
+	logger, _ = zap.NewProduction()
+}
+
+func main() {
+	directory := flag.String("directory", "", "The working directory")
+	//parallelism := flag.Int("parallelism", 10, "Threads to run")
+	version := flag.String("version", "latest", "Tofu version to run")
+
+	operation := os.Args[1]
+
+	_, err := chushi.New(tfe.DefaultConfig())
+	if err != nil {
+		logger.Fatal("failed to initialize chushi SDK", zap.Error(err))
+	}
+	ctx := context.Background()
+
+	tf, err := ensureTofu(*directory, *version)
+	if err != nil {
+		logger.Fatal("failed to install tofu", zap.Error(err))
+	}
+
+	if err = setup(tf, &bytes.Buffer{}); err != nil {
+		logger.Fatal("failed to setup execution", zap.Error(err))
+	}
+
+	if err = tf.Init(ctx, tfexec.Upgrade(false)); err != nil {
+		logger.Fatal("failed to initialize tofu", zap.Error(err))
+	}
+
+	switch operation {
+	case "plan":
+		err = opPlan(ctx, tf)
+	case "apply":
+	case "destroy":
+	}
+	// Parse our environment and pass it to the Tofu execution
+
+}
+
+func ensureTofu(workingDirectory string, tofuVersion string) (*tfexec.Terraform, error) {
+	installer := install.NewInstaller()
+
+	var source src.Source
+	if tofuVersion == "latest" {
+		source = &releases.LatestVersion{
+			Product: product.Tofu,
+		}
+	} else {
+		v, err := version.NewVersion(tofuVersion)
+		if err != nil {
+			return nil, err
+		}
+		source = &releases.ExactVersion{
+			Version: v,
+			Product: product.Tofu,
+		}
+	}
+
+	installation, err := installer.Ensure(context.TODO(), []src.Source{source})
+	if err != nil {
+		return nil, err
+	}
+	return tfexec.NewTerraform(workingDirectory, installation)
+}
+
+func setup(tf *tfexec.Terraform, output io.Writer) error {
+	envs := map[string]string{}
+	for _, envVar := range os.Environ() {
+		chunks := strings.Split(envVar, "=")
+		if len(chunks) != 2 {
+			continue
+		}
+		envs[chunks[0]] = chunks[1]
+	}
+	if err := tf.SetEnv(envs); err != nil {
+		return err
+	}
+	tf.SetStdout(output)
+	tf.SetStderr(output)
+	return nil
+}
+
+func opPlan(ctx context.Context, tf *tfexec.Terraform) error {
+	planOnly := flag.Bool("plan-only", false, "Only run a plan operation")
+	// This is hacky. We'll want to move to a full list of string slices
+	targets := flag.String("targets", "", "Target addresses")
+	opts := []tfexec.PlanOption{
+		tfexec.Out("tfplan"),
+	}
+	if *planOnly {
+		opts = append(opts, tfexec.Lock(false))
+	}
+	if *targets != "" {
+		targetAddrs := strings.Split(*targets, ",")
+		for _, targetAddr := range targetAddrs {
+			opts = append(opts, tfexec.Target(targetAddr))
+		}
+	}
+	hasChanges, err := tf.Plan(ctx, opts...)
+	if err != nil {
+		return err
+	}
+
+	if hasChanges {
+		fmt.Println("Changes occured!")
+	}
+	// Upload the plan output
+	// Convert the JSON plan
+	// Update the plan status
+	return nil
+}
